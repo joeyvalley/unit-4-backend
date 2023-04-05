aa8869f1450d9a755dc170cbc79ccd031f13ad77
Joseph Valle <josephrvalle@gmail.com>
2023-04-05 16:24:04 -0400

    no simple jwt

--- project/settings.py
+++ project/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist',
+    # 'rest_framework_simplejwt',
+    # 'rest_framework_simplejwt.token_blacklist',
+    'rest_framework.authtoken',
     'app',
 ]
 
