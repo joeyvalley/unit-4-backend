f5ef94cd6cf8175045dfab8b1fb1175230440dbc
Jazsadevee <jazsadevee@gmail.com>
2023-04-10 12:48:07 -0400

    remove dupicated likes from the same user

--- app/views.py
+++ app/views.py
@@ -273,14 +273,15 @@ def like(request):
     post = Post.objects.get(id=post_id)
     user_id = request.data['user_id']
     if user_id in post.liked_by:
-        post.liked_by.remove(user_id)
-        post.save()
-        print("removed like")
         return Response({'current like': post.liked_by})
-    post.liked_by.append(user_id)
-    post.save()
-    print()
-    return Response({'current like': post.liked_by})
+
+    # post.liked_by.remove(user_id)
+    #     post.save()
+    #     print("removed like")
+    #     return Response({'current like': post.liked_by})
+    # post.liked_by.append(user_id)
+    # post.save()
+    # print()
 
 
 @api_view(['POST'])
